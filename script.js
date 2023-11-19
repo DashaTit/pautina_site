a4beb7f722e4dc515fa4339eb8c744497686cee2
Arrya <107953441+DashaTit@users.noreply.github.com>
2023-11-19 16:39:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// const slider = document.querySelectorAll('#slider'),
+// pack = document.querySelectorAll('.skarlett'),
+// btn_prev = document.querySelector('.icon1'),
+// btn_next = document.querySelector('.icon2');
+
+// let slidercount = 0;
+
+// btn_next.addEventListener('click', next_slide);
+// btn_prev.addEventListener('click', prev_slide);
+
+// function next_slide() {
+//   slidercount++;
+//   if (slidercount >= 2) slidercount = 0;
+// }
+
+// function prev_slide() {
+//   slidercount--;
+//   if (slidercount < 0) slidercount = 2;
+// }
+
+// function roll_slider() {
+//   pack.style.transform = 'translateX(${-slidercount * 760}px)';
+// }
